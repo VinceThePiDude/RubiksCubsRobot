5daebd66698c85f9384fb8a2e78db21cc768cb86
Shadoe <kaustubhkarthik@gmail.com>
2021-06-15 20:16:42 +0100

    Update main.py

--- PC(Python)/main.py
+++ PC(Python)/main.py
@@ -1,18 +1,20 @@
 import kociemba
 from scrambler import get_cube_string
+from scrambleGenerator import gen_scramble
 
 def main():
     print("STARTING!")
-    scramble = input("Enter Scramble: ")
+    scramble = gen_scramble()[:-4]
     cube = get_cube_string(scramble).upper()
     cube = cube.replace("G", "F")
     cube = cube.replace("W", "U")
     cube = cube.replace("Y", "D")
     cube = cube.replace("O", "L")
-    print(cube)
-    print(kociemba.solve(cube)) # 54 letters as labled in notation.txt (example of solver : UUUUUUUUURRRRRRRRRFFFFFFFFFDDDDDDDDDLLLLLLLLLBBBBBBBBB )
+    print("Scramble: " + scramble)
+    print("Solution: " + kociemba.solve(cube)) # 54 letters as labled in notation.txt (example of solver : UUUUUUUUURRRRRRRRRFFFFFFFFFDDDDDDDDDLLLLLLLLLBBBBBBBBB )
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
+

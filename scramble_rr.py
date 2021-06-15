66248d4dc674aaf3050d3bcb08b71a340a0c6945
Shadoe <kaustubhkarthik@gmail.com>
2021-06-15 19:59:42 +0100

    Update scramble_rr.py

--- scramble_rr.py
+++ scramble_rr.py
@@ -1,16 +1,21 @@
 import pycuber as pc
+import kociemba
 
-vis_cube = [] * 36
-scramble = "z2 R U R' U' R' F R2 U' R' U' R U R' F'"
 # Cube
-cube = pc.Cube()
-cube = str(cube(scramble))
-ls_cube = [val for val in list(cube) if val != ' ' and val != '[' and val != ']' and val != '\n']
-U = ''.join(ls_cube[:9])
-D = ''.join(ls_cube[-9:])
-middle_cube = ls_cube[9:-9]
-middle_cube = ''.join(sum([middle_cube[i + j : i + j + 3] for i in range(0, 12, 3) for j in range(0, 25, 12)], []))
-str_cube = U + middle_cube[18: 27] + middle_cube[9: 18] + D + middle_cube[0:9] + middle_cube[27:36]
-print(str_cube)
+def get_cube_string(scramble):
+    # Gets cube visualisation
+    vis_cube = pc.Cube()
+    vis_cube = str(vis_cube("z2 " + scramble))
 
+    # Filters the string for letters(colors) only
+    ls_cube = [val for val in list(vis_cube) if val != ' ' and val != '[' and val != ']' and val != '\n']
+    U = ''.join(ls_cube[:9])
+    D = ''.join(ls_cube[-9:])
+    middle_cube = ls_cube[9:-9]
 
+    # Sorts the middle part of the cube into L, F, R, B
+    middle_cube = ''.join(sum([middle_cube[i + j : i + j + 3] for i in range(0, 12, 3) for j in range(0, 25, 12)], []))
+
+    # Formats it for solver
+    str_cube = U + middle_cube[18: 27] + middle_cube[9: 18] + D + middle_cube[0:9] + middle_cube[27:36]
+    return str_cube

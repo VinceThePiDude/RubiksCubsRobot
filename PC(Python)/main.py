07862cc7f03b9efb4ccdc39138e7a3fa6aa653dd
VinceThePiDude <44037261+VinceThePiDude@users.noreply.github.com>
2021-06-15 20:19:54 +0100

    Update main.py

--- PC(Python)/main.py
+++ PC(Python)/main.py
@@ -1,19 +1,22 @@
 import kociemba
 from scrambler import get_cube_string
+from scrambleGenerator import gen_scramble
+
 import serial
 import time
 
 def main():
     print("STARTING!")
     arduino = serial.Serial(port='/dev/ttyACM0', baudrate=115200, timeout=.1)
-    scramble = input("Enter Scramble: ")
+    scramble = gen_scramble()[:-4]
     cube = get_cube_string(scramble).upper()
     cube = cube.replace("G", "F")
     cube = cube.replace("W", "U")
     cube = cube.replace("Y", "D")
     cube = cube.replace("O", "L")
-    print(cube)
-    print(kociemba.solve(cube)) # 54 letters as labled in notation.txt (example of solver : UUUUUUUUURRRRRRRRRFFFFFFFFFDDDDDDDDDLLLLLLLLLBBBBBBBBB )
+    print("Scramble: " + scramble)
+    print("Solution: " + kociemba.solve(cube)) # 54 letters as labled in notation.txt (example of solver : UUUUUUUUURRRRRRRRRFFFFFFFFFDDDDDDDDDLLLLLLLLLBBBBBBBBB )
+
 
 
 def write_read(x):

641cb8de3ac319ade8628519d358eceb634bb7a6
Shadoe <kaustubhkarthik@gmail.com>
2021-06-19 00:06:37 +0100

    Update main.py

--- PC(Python)/main.py
+++ PC(Python)/main.py
@@ -9,10 +9,6 @@ def main():
     arduino = serial.Serial(port='/dev/ttyACM0', baudrate=115200, timeout=.1)
     scramble = gen_scramble()[:-4]
     cube = get_cube_string(scramble).upper()
-    cube = cube.replace("G", "F")
-    cube = cube.replace("W", "U")
-    cube = cube.replace("Y", "D")
-    cube = cube.replace("O", "L")
     print("Scramble: " + scramble)
     print("Solution: " + kociemba.solve(cube)) # 54 letters as labled in notation.txt (example of solver : UUUUUUUUURRRRRRRRRFFFFFFFFFDDDDDDDDDLLLLLLLLLBBBBBBBBB )
 
